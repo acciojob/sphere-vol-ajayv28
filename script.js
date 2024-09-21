e168004ad3ca1a65df04160202eea001f1c92f70
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 12:50:13 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function volume_sphere() {
     //Write your code here
+	
+    event.preventDefault();
+
+    const radiusInput = document.getElementById("radius").value;
+
+    const radius = parseFloat(radiusInput);
+
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = 'NaN';
+        return;
+    }
+
+
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    const roundedVolume = volume.toFixed(4);
+
+   
+    document.getElementById("volume").value = roundedVolume;
+
   
 } 
 
